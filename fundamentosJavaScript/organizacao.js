21c5fd75f78c3c1e1ad99dec8de19d77a1665a98
Pedro Fortunato <pedrofort28@gmail.com>
2025-03-11 22:45:38 -0300

    Fundamentos JavaScript: Organização

---
+++ fundamentosJavaScript/organizacao.js
@@ -0,0 +1,10 @@
+// JavaScript é organizado em sentenças de código que podem ou não terminar com ponto e vírgula.
+// Também é organizado em blocos de código que são delimitados por chaves.
+console.log("Sentença de código.");
+
+{
+    {
+        console.log("Olá!");
+        console.log("Mundo!"); // Padrão do curso
+    }
+}
