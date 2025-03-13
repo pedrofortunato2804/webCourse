2e4cf3615a5f42956fbd403348b0f28013b73a9d
Pedro Fortunato <pedrofort28@gmail.com>
2025-03-13 10:19:29 -0300

    Fundamentos JavaScript: Variables and Constants

---
+++ fundamentosJavaScript/variaveisEConstantes.js
@@ -0,0 +1,17 @@
+var a = 3; // Variável 
+let b = 2; // Variável
+
+var a = 30;
+// let b = 20; Não posso declarar novamente uma variável com let
+b = 20; // Posso atribuir um novo valor a uma variável let
+
+console.log(a, b);
+
+a = 300;
+b = 400;
+
+console.log(a, b);
+
+const c = 5; // Constante, não posso atribuir um novo valor, ou seja, não muda ao decorrer do código.
+// c = 50;  Não posso atribuir um novo valor a uma constante
+console.log(c);
