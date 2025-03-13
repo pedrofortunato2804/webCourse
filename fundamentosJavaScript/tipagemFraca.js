5acb6ff57f814d4489f380850a2b0b5bed334562
Pedro Fortunato <pedrofort28@gmail.com>
2025-03-13 10:19:03 -0300

    Fundamentos JavaScript: Tipagem Fraca

---
+++ fundamentosJavaScript/tipagemFraca.js
@@ -0,0 +1,13 @@
+let qualquer = 'Legal';
+console.log(qualquer);
+console.log(typeof qualquer);
+
+qualquer = 3.1415;
+console.log(qualquer);
+console.log(typeof qualquer);
+// Mudar as variáveis de tipo é uma característica da tipagem fraca.
+
+// Evitar nome genéricos e siglas para variáveis e constantes.
+let valor = '';
+let numero = 1;
+let pqp = false; // Produto Químico Perigoso... kkkkkkkkkkkkkkkkkkkk
