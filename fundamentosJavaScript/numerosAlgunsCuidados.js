7f9787d68eadec9e039ce2e1e01184dd5dae0112
Pedro Fortunato <pedrofort28@gmail.com>
2025-03-13 10:18:13 -0300

    Fundamentos JavaScript: Numbers Cuidados

---
+++ fundamentosJavaScript/numerosAlgunsCuidados.js
@@ -0,0 +1,7 @@
+console.log(7/0); // Infinity
+console.log('10'/2); // Funciona
+console.log("Show!" * 2); //NaN
+console.log(0.1 + 0.7); // Imprecisão
+// console.log(10.toString());
+console.log((10.345).toFixed(2));
+console.log(10.345.toFixed(2));
