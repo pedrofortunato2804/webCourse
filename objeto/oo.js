f6a17be525d46913bae1ce25e61367b53aba9029
Pedro Fortunato <pedrofort28@gmail.com>
2025-04-01 11:37:23 -0300

    Objetos JavaScript: Iniciando.

---
+++ objeto/oo.js
@@ -0,0 +1,24 @@
+// CÓDIGO NÂO EXECUTÁVEL.
+
+// Procedural
+processamento(valor1, valor2, valor3)
+
+// OO
+
+objeto = {
+    valor1,
+    valor2,
+    valor3,
+    processamento(){
+        // ...
+    }
+}
+
+objeto.processamento() //Foco passou a ser o objeto
+
+
+// Pricipios importantes.
+// 1) Abstração = Pegar algo do mundo real e trazer para dentro do sistema.
+// 2) Encapsulamento = Interface simples para usar o objeto.
+// 3) Herança (protótipo) = recebe caracteristicas e atributos de um objeto pai.
+// 4) Polimorfismo = Várias formas.
